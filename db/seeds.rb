259db23f66eec0bf976ac10d1253d308c7aa7aab
Pierre Liduena <assurancesvelo@gmail.com>
2017-10-17 16:43:29 +0200

    sup fonction

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,17 @@
 Cyclo.destroy_all
 
-Cyclo.create!(name: 'La Pierre Chany', photo: 'header-slide1.jpg', date: '11/11/2017', address:'Langeac')
-Cyclo.create!(name: 'La Pierre Chany', photo: 'header-slide1.jpg', date: '11/11/2017', address:'Langeac')
-Cyclo.create!(name: 'La Pierre Chany', photo: 'header-slide1.jpg', date: '11/11/2017', address:'Langeac')
-Cyclo.create!(name: 'La Pierre Chany', photo: 'header-slide1.jpg', date: '11/11/2017', address:'Langeac')
-Cyclo.create!(name: 'La Pierre Chany', photo: 'header-slide1.jpg', date: '11/11/2017', address:'Langeac')
-Cyclo.create!(name: 'La Pierre Chany', photo: 'header-slide1.jpg', date: '11/11/2017', address:'Langeac')
-Cyclo.create!(name: 'La Pierre Chany', photo: 'header-slide1.jpg', date: '11/11/2017', address:'Langeac')
-Cyclo.create!(name: 'La Pierre Chany', photo: 'header-slide1.jpg', date: '11/11/2017', address:'Langeac')
-Cyclo.create!(name: 'La Pierre Chany', photo: 'header-slide1.jpg', date: '11/11/2017', address:'Langeac')
-Cyclo.create!(name: 'La Pierre Chany', photo: 'header-slide1.jpg', date: '11/11/2017', address:'Langeac')
-Cyclo.create!(name: 'La Pierre Chany', photo: 'header-slide1.jpg', date: '11/11/2017', address:'Langeac')
+Cyclo.create!(name: 'La Jacques Gouin', photo: 'photo-cyclo1.jpg', date: '04/03/2018', address: 'Le Parc de Villeroy')
+Cyclo.create!(name: 'La Morvandelle', photo: 'photo-cyclo2.jpg', date: '07/04/2018', address: 'La Grande Verrière')
+Cyclo.create!(name: 'La Velostar', photo: 'photo-cyclo3.jpg', date: '01/05/2018', address:'Breuillet')
+Cyclo.create!(name: 'Les Boucles de la Marne', photo: 'photo-cyclo4.jpg', date: '27/05/2018', address:'Montmirail')
+Cyclo.create!(name: 'La Claudio Chiappucci', photo: 'photo-cyclo5.jpg', date: '02/06/2018', address:'Arnay-le-Duc')
+Cyclo.create!(name: 'Les Copains Cyfac', photo: 'photo-cyclo6.jpg', date: '04/03/2018', address:'Ambert')
+Cyclo.create!(name: 'La Jacques Gouin', photo: 'photo-cyclo7.jpg', date: '30/06/2018', address:'Le Parc de Villeroy')
+Cyclo.create!(name: 'La Perigordine', photo: 'photo-cyclo8.jpg', date: '08/07/2018', address:'Montignac-Lascaux')
+Cyclo.create!(name: 'La Campilaro', photo: 'photo-cyclo9.jpg', date: '20/08/2018', address:'Argelès-Gazost')
+Cyclo.create!(name: 'La Pierre Chany', photo: 'photo-cyclo10.jpg', date: '04/08/2018', address:'Langeac')
+Cyclo.create!(name: 'La Jean François Bernard', photo: 'photo-cyclo11.jpg', date: '19/08/2018', address:'Corbigny')
+Cyclo.create!(name: 'Les Monts de l\'aixois', photo: 'photo-cyclo12.jpg', date: '09/09/2018', address:'Semur en Auxois')
 
 Recomandation.destroy_all
 
