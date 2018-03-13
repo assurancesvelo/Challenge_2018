01a837eef7f9973438769614e88c95eb4d50a4d7
assurancesvelo <assurancesvelo@gmail.com>
2018-03-13 12:37:34 +0100

    c
    c

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :lots, only: [:index, :show]
   get "/reglement" => "pages#reglement"
+  get "/classement" => "pages#classement"
 end
